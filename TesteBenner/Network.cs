e2b4e80bad2323d62015cda475c8db07d4412a73
Jonathan <bauer.jonathan.jj@gmail.com>
2025-07-31 22:32:39 -0300

    Adicionar arquivos de projeto.

---
+++ TesteBenner/Network.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TesteBenner
+{
+    public class Network
+    {
+        private readonly int size;
+        private readonly List<HashSet<int>> grupos;
+
+        public Network(int size)
+        {
+            if (size <= 0)
+            {
+                throw new Exception("O número deve ser maior que zero.");
+            }
+
+            this.size = size;
+            this.grupos = new List<HashSet<int>>();
+
+            for (int i = 1; i <= size; i++)
+            {
+                grupos.Add(new HashSet<int> { i });
+            }
+        }
+
+        public void Connect(int a, int b)
+        {
+            Validate(a, b);
+
+            var grupoA = grupos.FirstOrDefault(g => g.Contains(a));
+            var grupoB = grupos.FirstOrDefault(g => g.Contains(b));
+
+            if (grupoA != grupoB)
+            {
+                grupoA.UnionWith(grupoB);
+                grupos.Remove(grupoB);
+            }
+        }
+
+        public bool Query(int a, int b)
+        {
+            return grupos.Any(g => g.Contains(a) && g.Contains(b));
+        }
+
+        private void Validate(int a, int b)
+        {
+            if (a < 1 || a > size || b < 1 || b > size)
+            {
+                throw new ArgumentOutOfRangeException("Os números devem ser de 1 até " + size);
+            }
+        }
+    }
+}
---
+++ TesteBenner/Program.cs
@@ -0,0 +1,17 @@
+﻿using TesteBenner;
+
+var network = new Network(size: 8);
+
+network.Connect(1, 2);
+network.Connect(6, 2);
+network.Connect(2, 4);
+network.Connect(5, 8);
+
+Console.WriteLine(network.Query(1, 6));
+Console.WriteLine(network.Query(6, 4));
+Console.WriteLine(network.Query(7, 4));
+Console.WriteLine(network.Query(5, 6));
+Console.WriteLine(network.Query(2, 3));
+Console.WriteLine(network.Query(3, 4));
+Console.WriteLine(network.Query(2, 6));
+Console.WriteLine(network.Query(8, 5));
